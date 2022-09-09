cb9482cc6d3f52167a7b1a81fd526f9cf01b7378
joyogbu <obyjoy@yahoo.com>
2022-09-08 19:49:24 -0700

    new files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	
+	printf("Size of a char: %lu byte (s)\n", (unsigned long)sizeof(a));
+}

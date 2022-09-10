2703ca865befd12238d1ea4c1f4b688c0f542320
joyogbu <obyjoy@yahoo.com>
2022-09-10 16:28:55 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_basic16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int h;
+	int i;
+	
+	for (h = 0; h <= 9; h++)
+	{
+		for (i = 'a'; i <= 'f'; i++;)
+		{
+			putchar(i);
+		}
+		putchar(h);
+	}
+	putchar('\n');
+	return (0);
+}

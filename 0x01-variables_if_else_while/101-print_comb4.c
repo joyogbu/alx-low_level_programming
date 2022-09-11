ad5b7ec38294d37a54e2a2df28a796b70da18bc0
joyogbu <obyjoy@yahoo.com>
2022-09-11 16:25:40 -0700

    task 1 and 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
 				putchar((k % 10) + '0');
-				if ((i == 7) && (j == 8) (k == 9))
+				if ((i == 7) && (j == 8) && (k == 9))
 				{
 					continue;
 				}

0d84bc0ccfe5af1ad88b5c1152836db4a5bf2eb0
joyogbu <obyjoy@yahoo.com>
2022-09-11 09:13:50 -0700

    corrected 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,11 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
-			if ((i == 9) && (j == 9))
+			if ((i == 8) && (j == 9))
 			{
 				continue;
 			}

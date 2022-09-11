1c6395aa40d1f17a3bf0b9eaece31832e3c90d43
joyogbu <obyjoy@yahoo.com>
2022-09-11 07:00:39 -0700

    ninth task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib h>
+
+/**
+ * main - prints all possible combination of si gle digits numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

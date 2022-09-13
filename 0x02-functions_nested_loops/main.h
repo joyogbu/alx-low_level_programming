a35a3b603896347d5ec6330ec0e022fac6fce01f
joyogbu <obyjoy@yahoo.com>
2022-09-13 09:38:07 -0700

    first commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file - header file
+ *
+ * Description: containing all the prototypes of functions used in the project
+ */
+
+int _putchar(char c);
+#endif

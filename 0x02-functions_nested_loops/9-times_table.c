d598e9b0f0e21bb6d055c3bb6cebcdf10ecfe4e0
joyogbu <obyjoy@yahoo.com>
2022-09-14 04:09:36 -0700

    task 9 edited

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,20 +11,32 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar('0');
-		for (j = 1; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
+			k = i + j;
+			if ((k / 10) == 0)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(k + '0');
+				if (j == 9)
+				{
+					continue;
+				}
 			_putchar(' ');
 			_putchar(',');
-			k = i * j;
-			if (k <= 9)
-			{
-				_putchar(' ');
-			}
 			else
 			{
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
+				if (j == 9)
+				{
+					continue;
+				}
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');

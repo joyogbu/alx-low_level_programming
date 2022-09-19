3ff74010c759175795792d3e339e3b424d58e5d3
joyogbu <obyjoy@yahoo.com>
2022-09-19 10:30:51 -0700

    ytask3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: input string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

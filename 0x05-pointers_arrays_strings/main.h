905f2761f13919b2ce3ed63ae1c1365584147dc4
joyogbu <obyjoy@yahoo.com>
2022-09-19 11:54:32 -0700

    edited task 3and4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,4 +10,4 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char _*strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);

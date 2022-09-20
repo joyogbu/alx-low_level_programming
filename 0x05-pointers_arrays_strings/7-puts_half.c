5006fdfaf3393e855d38810dbcd02a5d5ccdafb5
joyogbu <obyjoy@yahoo.com>
2022-09-20 00:28:55 -0700

    task no8 update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,17 +9,19 @@ void puts_half(char *str)
 {
 	int j = 0, n;
 
-	while (str[j] != 0)
+	while (str[j] != '\0')
 	{
 		j++;
 	}
 	if (j % 2 == 0)
 	{
 		n = j / 2;
-}
+	}
 	else
 		n = (j - 1) / 2;
-	for (j = n; str[j] != 0; j++)
-	_putchar(str[j]);
+	for (j = n; str[j] != '\0'; j++)
+	{
+		_putchar(str[j]);
+	}
 	_putchar('\n');
 }

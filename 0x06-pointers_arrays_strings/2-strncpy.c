94f37d4258661de2d58e8abb1b9f8a075c370c3c
joyogbu <obyjoy@yahoo.com>
2022-09-22 05:36:39 -0700

    copying a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,9 +5,11 @@
  * _strncpy - function that copy's a string
  * @dest: an input s5ring
  * @src: an input string
+ * @n: an input integer
+
  * Return: a pointer to the dest
  */
-char *_strncpy(char *dest, char *src, intn)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
 	int i = 0;

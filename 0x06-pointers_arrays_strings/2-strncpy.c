fcecc77728ca61a62be16389c8a0617e09d29bb0
joyogbu <obyjoy@yahoo.com>
2022-09-22 05:30:21 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - function that copy's a string
+ * @dest: an input s5ring
+ * @src: an input string
+ * Return: a pointer to the dest
+ */
+char *_strncpy(char *dest, char *src, intn)
+{
+	int j = 0;
+	int i = 0;
+	char *pt1 = dest;
+	char *pt2 = src;
+
+	while (*src)
+	{
+		j++;
+		src++;
+	}
+	j++;
+	if (n > j)
+	{
+		n = j;
+	}
+	src = pt2;
+	for (; i < n; i++)
+	{
+		*dest++ = *src++;
+	}
+	return (pt1);
+}

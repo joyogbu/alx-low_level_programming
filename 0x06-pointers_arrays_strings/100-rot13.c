5ab5e0fadfff06c2dc8ed477ed137b3378a0a4da
joyogbu <obyjoy@yahoo.com>
2022-09-22 19:17:00 -0700

    rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - function that string using rot13
+ * @s: an input string
+ * Return: an encoded string
+ */
+char *rot13(char *s)
+{
+	int j = 0;
+
+	while (s[j] != '\0')
+	{
+		while ((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= 'A' && s[j] <= 'Z'))
+		{
+			if ((s[j] >= 'a' && s[j] <= 'm') || (s[j] >= 'A' && s[j] <= 'M'))
+			{
+				s[j] += 13;
+			}
+			else
+			{
+				s[j] -= 13;
+			}
+			j++;
+		}
+		j++;
+	}
+	return (s);
+}

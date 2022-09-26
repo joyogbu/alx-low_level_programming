df35c16feb85e02017d1ceb6240e0c5591a172a9
joyogbu <obyjoy@yahoo.com>
2022-09-26 14:26:25 -0700

    memory_copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *start = src;
+	/** char *start = src; */
 	char *end = dest;
+	unsigned int i;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}

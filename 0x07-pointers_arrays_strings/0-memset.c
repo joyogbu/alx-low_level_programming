8ebbce4fe34bebb07119eb14179370c30bb9a3e1
joyogbu <obyjoy@yahoo.com>
2022-09-26 08:32:59 -0700

    task 0 correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,9 +12,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	char *start;
+	char *start = s;
 
-	for (i = 0; i < n; i++;)
+	for (i = 0; i < n; i++)
 		*s = b;
 	return (start);
 }

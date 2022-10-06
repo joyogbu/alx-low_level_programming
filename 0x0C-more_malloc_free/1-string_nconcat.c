2166a9bc174f7e6e500a0af2f7dc62f33f1c7be5
joyogbu <obyjoy@yahoo.com>
2022-10-06 07:35:16 -0700

    task 1, nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - function that concatenates two strings
+ * s1 and n bytes from s2
+ * @s1: an input string
+ * @s2: an input string
+ * @n: number of bytes from s2
+ * Return: NULL if the function fails,
+ * otherwise returns a pointer to the newly allocated space
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	char *sptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+	{
+		i++;
+	}
+	while (s2[j])
+	{
+		j++;
+	}
+	sptr = malloc(sizeof(char) * (i + n + 2));
+	if (sptr == NULL)
+	{
+		return (NULL);
+	}
+	if (n >= j)
+	{
+		n = j;
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		sptr[k] = s1[i];
+		j++;
+		k++;
+	}
+	for (j = 0; j <= n; j++)
+	{
+		sptr[k] = s2[j];
+		k++;
+	}
+	sptr[k] = '\0';
+	return (sptr);
+}
+

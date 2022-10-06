636f67994a26f01a95387db49c9ee3d88f39efe1
joyogbu <obyjoy@yahoo.com>
2022-10-06 05:58:31 -0700

    correction on task3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,7 +28,7 @@ char *str_concat(char *s1, char *s2)
 		i++;
 		j++;
 	}
-	spt = malloc(sizeof(char) * (i + j + 1));
+	spt = malloc(sizeof(char) * (i + j + 2));
 	if (s1 == NULL || !spt)
 	{
 		return (NULL);

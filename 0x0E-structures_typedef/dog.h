cbcd08121eea9fd6f46e123e5cc6cb6182ed1285
joyogbu <obyjoy@yahoo.com>
2022-10-10 10:08:16 -0700

    task 0 correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog 
+/**
+ * struct dog - struct function
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: a structure with 3 members
+ */
+struct dog
 {
 	char *name;
 	float age;

3191b942b9f6ccc5cceef4a3d8e861a20e146570
joyogbu <obyjoy@yahoo.com>
2022-10-10 06:19:16 -0700

    task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program to print the name of the file
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}

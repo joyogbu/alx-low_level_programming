318f60882c588d96b65aaa8ac674ae4dd029d14e
joyogbu <obyjoy@yahoo.com>
2022-10-11 13:58:08 -0700

    task4 new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: pounter to the newly created dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *nptr;
+	char *optr;
+
+	int i = 0;
+	int j = 0;
+	int k;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	for (i = 0; name[i];)
+		i++;
+	for (j = 0; owner[j];)
+		j++;
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	nptr = malloc(i + 1);
+	if (nptr == NULL)
+		return (NULL);
+	for (k = 0; name[k]; k++)
+		nptr[k] = name[k];
+	nptr[k] = '\0';
+	optr = malloc(j + 1);
+	if (optr == NULL)
+		return (NULL);
+	for (k = 0; owner[k]; k++)
+		optr[k] = owner[k];
+	optr[k] = '\0';
+	new_dog->name = nptr;
+	new_dog->age = age;
+	new_dog->owner = optr;
+	return (new_dog);
+}

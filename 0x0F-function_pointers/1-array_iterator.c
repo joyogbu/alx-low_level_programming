ca1769eb5d5e04e03f8b770116971af99d624321
joyogbu <obyjoy@yahoo.com>
2022-10-12 08:03:10 -0700

    task 0 and 1 correction

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,9 +6,10 @@
  * given as an array on each element of the array
  * @array: an input array
  * @size: array size
+ * @action: pointer to the array elements
  * Return: nothing
  */
-void array_iterator(int *array, size_t size void(*action)(int))
+void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	int i;
 

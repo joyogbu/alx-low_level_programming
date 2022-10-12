5f04e697667735487872941ebdc3d840657beef4
joyogbu <obyjoy@yahoo.com>
2022-10-12 07:48:35 -0700

    function pointers task 0 and 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that prints name
+ * @name: name to print
+ * @f: pointer to the name
+ * Return: nothing
+ */
+void print_name(char *name, void(*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - function that executes a function
+ * given as an array on each element of the array
+ * @array: an input array
+ * @size: array size
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size void(*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}

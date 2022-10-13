bc4b9c9b1df93aa06980b3613242cf951be12aa9
joyogbu <obyjoy@yahoo.com>
2022-10-13 14:09:51 -0700

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d ", numbers);
 		if (separator == NULL)
 			continue;
-		printf("%d", separator);
+		printf("%s", separator);
 	}
 		va_end(args);
 		printf("\n");

9a4d758767c58f61b54235361278b23e3116664d
joyogbu <obyjoy@yahoo.com>
2022-10-13 14:27:48 -0700

    task1 updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,8 +22,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d ", numbers);
 		if (separator == NULL)
 			continue;
-		printf("%s", separator);
+		if (i < n - 1)
+			printf("%s", separator);
 	}
-		va_end(args);
 		printf("\n");
+		va_end(args);
 }

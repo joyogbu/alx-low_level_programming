858b60e0902a88c3f853646031d26f60943f6ff6
joyogbu <obyjoy@yahoo.com>
2022-10-20 17:03:19 -0700

    task 2 correction

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,11 +12,11 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t* new_node;
+	list_t *new_node;
 	char *result;
-	int i = 0; 
+	int i = 0;
 
-	new_node = (list_t*)malloc(sizeof(list_t));
+	new_node = (list_t *)malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 	result = strdup(str);

4f88b19d2bbbffebc5a8da8f622db795dd24790d
joyogbu <obyjoy@yahoo.com>
2022-10-21 15:04:25 -0700

    yask 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * first - prints a string before the main function is executed
+ * Return: nothing
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

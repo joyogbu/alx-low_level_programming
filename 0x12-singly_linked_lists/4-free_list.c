fa91f350a7dfcfdf3ffeab48635e48a6a59b9c75
joyogbu <obyjoy@yahoo.com>
2022-10-21 12:21:53 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_list - function that frees list_t list
+ * @h: list heas node
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+	temp = head;
+	head = head->next;
+	free(temp);
+}

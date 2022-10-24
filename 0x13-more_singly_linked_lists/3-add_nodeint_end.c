3ee70dbf13cb9281eba617b13683c081705654dc
joyogbu <obyjoy@yahoo.com>
2022-10-24 13:04:38 -0700

    tadk 2 3 correction

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,12 +5,15 @@
 /**
  * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: pointer to the node head
+ * @n: node element
  * Return: a pointer to the new nodevor NULL on failure
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
+
 	listint_t *tail = *head;
+
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);

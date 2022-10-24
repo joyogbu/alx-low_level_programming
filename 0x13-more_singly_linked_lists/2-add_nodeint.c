790fd1f953695429a08f018fbd26486e0b8fe373
joyogbu <obyjoy@yahoo.com>
2022-10-24 12:56:54 -0700

    task 3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
-	new_node =(listint_t *)malloc(sizeof(listint_t));
+	new_node = (listint_t *)malloc(sizeof(listint_t));
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to the node head
+ * Return: a pointer to the new nodevor NULL on failure
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	listint_t *tail = *head;
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		/** *tail = new_node; **/
+		return (*head);
+		tail = new_node;
+	}
+	while (tail->next != NULL)
+	{
+		tail = tail->next;
+	}
+	tail->next = new_node;
+	tail = new_node;
+	return (tail);
+}

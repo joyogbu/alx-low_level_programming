17c77d419c2223150587503f410b0187d3b0848b
joyogbu <obyjoy@yahoo.com>
2022-10-25 13:48:49 -0700

    task 9correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,12 +20,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	if (*head == NULL || idx == 0)
+	if (*head == NULL)
 	{
-		new_node->n =n;
-		new_node->next = *head;
+		new_node->n = n;
+		new_node->next = NULL;
 		*head = new_node;
 		return (*head);
+	}
+	if (idx == 0)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 	for (i = 0; temp != NULL && i < idx - 1; i++)
 	{

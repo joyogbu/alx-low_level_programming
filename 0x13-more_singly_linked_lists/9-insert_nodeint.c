037a46bb593119485337f6963d42bf5564d5f513
joyogbu <obyjoy@yahoo.com>
2022-10-25 13:56:16 -0700

    task9 update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	for (i = 0; temp != NULL && i < idx; i++)
+	for (i = 0; temp != NULL && i < idx - 1; i++)
 	{
 		temp = temp->next;
 	}

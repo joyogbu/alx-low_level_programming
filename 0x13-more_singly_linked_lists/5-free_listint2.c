4e28460f2256f5bb613104333f7a3039770e181b
joyogbu <obyjoy@yahoo.com>
2022-10-26 01:59:50 -0700

    task 5edit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,14 +12,13 @@ void free_listint2(listint_t **head)
 
 	if (*head == NULL)
 		return;
-	if (**head ==NULL)
-		return;
 	temp = *head;
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
+		temp->next = NULL;
 	}
 	*head = NULL;
 	head = NULL;

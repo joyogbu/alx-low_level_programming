976286db156acdd97efbfc76418cb5f4297aa08e
joyogbu <obyjoy@yahoo.com>
2022-10-26 01:48:28 -0700

    5correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL || (*head)->next = NULL)
+	if (*head == NULL || head = NULL)
 		return;
 	temp = *head;
 	while (*head)

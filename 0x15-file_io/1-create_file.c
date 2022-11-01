e2a678aae75b0646ea7075794cfe6c48da6582c6
joyogbu <obyjoy@yahoo.com>
2022-10-31 19:31:54 -0700

    task 1 edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,14 +18,20 @@ int create_file(const char *filename, char *text_content)
 {
 	int f;
 	int w;
+	int i = 0;
 
 	if (filename == NULL)
 		return (-1);
 	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (f < 0)
 		return (-1);
-	w = write(1, text_content, f);
+	while (text_content[i])
+	{
+		i++;
+	}
+	w = write(f, text_content, i);
 	if (w < 0)
 		return (-1);
-	return (w);
+	close(f);
+	return (1);
 }

ed58a599ea1d2e5049012916faf6f2be4eb87c1b
joyogbu <obyjoy@yahoo.com>
2022-11-02 09:26:59 -0700

    task no3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,9 @@ int main(int argc, char *argv[])
 {
 	int f1, f2, r, w;
 	char *buffer;
+	mode_t oldmask;
 
+	oldmask = umask(S_IWGRP);
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
@@ -50,7 +52,7 @@ int main(int argc, char *argv[])
 		free(buffer);
 		exit(98);
 	}
-	f2 = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
+	f2 = open(argv[2], O_CREAT |O_WRONLY | O_TRUNC | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH	);
 	if (f2 < 0)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
@@ -80,5 +82,7 @@ int main(int argc, char *argv[])
 	close_file(f1);
 	close_file(f2);
 
+
+	umask(oldmask);
 	return (0);
 }

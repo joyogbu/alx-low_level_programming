a91efa18d6167d77b04010dc8070f1e09e1f5624
joyogbu <obyjoy@yahoo.com>
2022-12-08 14:41:40 -0800

    task

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * dlistint_len - returns the number of elements in a dlistint list
+ * @h: head node
+ * Return: no of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return(count);
+}

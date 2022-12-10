d24330a19eacd6f47ba23e7dd178d33e930391c4
joyogbu <obyjoy@yahoo.com>
2022-12-10 08:19:41 -0800

    corrected 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,15 +32,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->n = n;
-		new_node->next = NULL;
-		new_node->prev = *h;
+		new_node->next = *h;
+		new_node->prev = NULL;
 		*h = new_node;
 		return (new_node);
 	}
-	/*if (idx == 1)
-	{
-		return (add_dnodeint(h, n));
-	}*/
 	for (i = 0; temp != NULL && i < idx - 1; i++)
 	{
 		if (temp == NULL || temp->next == NULL)
@@ -48,10 +44,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 		temp = temp->next;
-	}
-	if (temp->next == NULL)
-	{
-		return (add_dnodeint_end(h, n));
 	}
 	while (temp != NULL && i < idx - 1)
 	{

6d96ede1acb841413172de8a0107e790c55c389f
joyogbu <obyjoy@yahoo.com>
2022-12-10 07:58:27 -0800

    no 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,6 +37,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
+	if (idx == 1)
+	{
+		return (add_dnodeint(h, n));
+	}
 	for (i = 0; temp != NULL && i < idx - 1; i++)
 	{
 		if (temp == NULL || temp->next == NULL)

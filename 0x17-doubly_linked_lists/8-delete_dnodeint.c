54a8b33481894eed2f8dca73f1945818aebe9cbc
joyogbu <obyjoy@yahoo.com>
2022-12-10 08:43:59 -0800

    task no 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,8 +11,8 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *temp;
-	listint_t *del;
+	dlistint_t *temp;
+	dlistint_t *del;
 
 	if (*head == NULL)
 		return (-1);

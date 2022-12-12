43492fd41f99692d36253bb6b06b60a4e4aef6f9
joyogbu <obyjoy@yahoo.com>
2022-12-12 13:11:39 -0800

    task 4

---
+++ 0x18-dynamic_libraries/demain.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int _puts();
+int main(void)
+{
+	_puts();
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/demo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int _puts()
+{
+	printf("--> Please make me win!\n");
+	return (0);
+}

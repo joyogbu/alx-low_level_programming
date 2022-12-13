1b09f710d73da11c7e7c071c43bca100631fb70c
joyogbu <obyjoy@yahoo.com>
2022-12-13 04:31:33 -0800

    testing2

--- 0x18-dynamic_libraries/demo.c
+++ 0x18-dynamic_libraries/demo.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 int _putss(void)
 {
 	char *str = "--> Please make me win!";
 	char *num = "9, 8, 10, 24, 75 9";
 	_putchar(*num);
 	_putchar(*str);
-	exit(0);
+	return (0);
 }

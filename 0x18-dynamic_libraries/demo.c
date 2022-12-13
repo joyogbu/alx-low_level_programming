0c984214d8c9263ab0a08ccd9fc2d541bd62c67c
joyogbu <obyjoy@yahoo.com>
2022-12-13 05:53:05 -0800

    test6

--- 0x18-dynamic_libraries/demo.c
+++ 0x18-dynamic_libraries/demo.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(void)
 {
-	char *str = "--> Please make me win!";
-	char *num = "9, 8, 10, 24, 75 9";
-	putchar(*num);
-	putchar(*str);
+	char *str = "Congratulations, you win the Jackpot!";
+	char *num = "9, 8, 10, 24, 75 - 9";
+	write(1, &num, 20);
+	write(1, &str, 37);
 	exit(0);
 }
